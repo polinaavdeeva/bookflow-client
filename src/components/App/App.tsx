330868d79d7df0139b1321a98012f5a34f86ef8c
Дубовой Олег <dubovoyo@inlinegroup-c.ru>
2024-04-07 22:49:11 +0300

    feat: структура проекта

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,10 +5,10 @@ import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
 const App: FC = (): React.ReactElement => {
   return (
     <Theme preset={presetGpnDefault}>
-    <p>
-      Платформа для обмена книгами между людьми в определённой местности ,где
-      можно оставлять отзывы о прочитанных книгах
-    </p>
+      <p>
+        Платформа для обмена книгами между людьми в определённой местности ,где
+        можно оставлять отзывы о прочитанных книгах
+      </p>
       <Button label="Войти" view="primary"/>
     </Theme>
     
