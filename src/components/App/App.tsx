de76a36448812aee96f18f3c14b3c3f98f2d1623
Дубовой Олег <dubovoyo@inlinegroup-c.ru>
2024-04-07 22:12:17 +0300

    feat: славянский ui-kit и зависимости

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,18 @@
 import React, { FC } from "react";
+import { Button } from '@consta/uikit/Button';
+import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
 
 const App: FC = (): React.ReactElement => {
   return (
+    <Theme preset={presetGpnDefault}>
     <p>
       Платформа для обмена книгами между людьми в определённой местности ,где
       можно оставлять отзывы о прочитанных книгах
     </p>
+      <Button label="Войти" view="primary"/>
+    </Theme>
+    
+
   );
 };
 
