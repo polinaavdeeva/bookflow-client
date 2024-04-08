d9d0a68f14aa852096bc5157199b926bfe0dd9d9
Дубовой Олег <dubovoyo@inlinegroup-c.ru>
2024-04-08 19:20:49 +0300

    feat: добавлены новые элменты

--- src/components/NavMenu/Menu.tsx
+++ src/components/NavMenu/Menu.tsx
@@ -1,19 +1,25 @@
 import { Link } from 'react-router-dom'
 import { Text } from '@consta/uikit/Text';
+import { Button } from '@consta/uikit/Button';
 import "./Menu.scss"
 
 const Menu = () => {
   return (
     <div className='menu-vertical'>
       <Text size='3xl'>BookFlow</Text>
-      <Link to=".">Главная</Link>
       <br/>
-      <Link to="mybooks">Мои книги</Link>
+      <Link to="."><Button label="Главная" view='clear' size='s' style={{width: "50vh", paddingRight: 170}}/></Link>
+      
+      <Link to="mybooks"><Button label="Мои книги" view='clear' size='s' style={{width: "50vh", paddingRight: 155}}/></Link>
       <br/>
-      <Link to="myprofile">Профиль</Link>
+      <Link to="myprofile"><Button label="Профиль" view='clear' size='s' style={{width: "50vh", paddingRight: 165}}/></Link>
       <br/>
-      <Link to="addbook">Добавть книгу</Link>
+      <Link to="addbook"><Button label="Добавить книгу" view='clear' size='s' style={{width: "50vh", paddingRight: 118}}/></Link>
       <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Button label="Выйти" view='clear' size='s' style={{width: "50vh", paddingRight: 180}}/>
     </div>
   )
 }
--- src/components/pages/MainPage/MainPage.tsx
+++ src/components/pages/MainPage/MainPage.tsx
@@ -1,10 +1,13 @@
 import { TextField } from '@consta/uikit/TextField';
 import { Select } from '@consta/uikit/Select';
 import { User } from '@consta/uikit/User';
+import { Card } from '@consta/uikit/Card';
+import { Text } from '@consta/uikit/Text';
 
 import { useState } from 'react';
 
 import "./MainPage.scss"
+import AdCard from '../../AdCard/AdCard';
 
 const MainPage = () => {
 
@@ -32,17 +35,27 @@ const MainPage = () => {
      
   return (
     <div>
-      <div style={{height: 76, width: '160vh', display: 'inline-block', margin: "16px 32px 16px 32px"}}>
+      <div style={{height: 32, width: '160vh', display: 'inline-block', margin: "16px 32px 16px 32px"}}>
         <TextField className='search'
 	        type="text"
-	        placeholder="Поиск"
+	        placeholder="Поиск" size="s"
         />
         
-        
-        <User avatarUrl="https://www.meme-arsenal.com/memes/7f7109497d0f562446e621e8e6073453.jpg" name="Райан Гослинг" info="Водитель" style={{ height: 30, float: 'inline-end' }}/>
-        <Select size='xs' items={items} value={value} onChange={setValue} style={{width: 91, height: 30, float: 'inline-end'}}/>
+        <User avatarUrl="https://www.meme-arsenal.com/memes/7f7109497d0f562446e621e8e6073453.jpg" name="Райан Гослинг" info="Водитель" style={{ width: 175, height: 30, float: 'inline-end' }}/>
+        <Select placeholder="Выберите город" view='clear' size='xs' items={items} value={value} onChange={setValue} style={{width: 140, height: 30, float: 'inline-end', marginTop: 4}}/>
       </div> 
+      <AdCard />
+
+      <Card verticalSpace="xl" horizontalSpace="xl" style={{marginTop: 10, marginLeft: 15, marginRight: 15, width: "158vh", height: 250, background:"#FFFBF5"}}>
+        <Text>Последнее Добавленное</Text>
+      </Card>
+
+      <Card verticalSpace="xl" horizontalSpace="xl" style={{marginTop: 10, marginLeft: 15, marginRight: 15, width: "158vh", height: 250, background:"#FFFBF5"}}>
+        <Text>Лучший рейтинг</Text>
+      </Card>
+
     </div>
+
   );
 };
 
