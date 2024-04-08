1100fa951a082b141b6c66225f68b5291e79bbed
Polina Avdeeva <polinaavdeeva55@gmail.com>
2024-04-08 23:47:47 +0300

    feat: добавлена страница входа

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { FC } from "react";
 
-import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
+import { Theme, presetGpnDefault } from "@consta/uikit/Theme";
 import { Route, Routes } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import MainPage from "../pages/MainPage/MainPage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import MyBooksPage from "../pages/MyBooksPage/MyBooksPage";
 
-
 const App: FC = (): React.ReactElement => {
   return (
     <Theme preset={presetGpnDefault}>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<MainPage/>} />
+          <Route index element={<MainPage />} />
           <Route path="mybooks" element={<MyBooksPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
---
+++ src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,59 @@
+import { Button } from "@consta/uikit/Button";
+import { TextField } from "@consta/uikit/TextField";
+import React, { FC } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./SignIn.scss";
+
+const SignIn: FC = (): React.ReactElement => {
+  return (
+    <section className="sign-up">
+      <div className="sign-up__background-logo" />
+      <div className="sign-up__content">
+        <div className="sign-up__content-container">
+          <div className="sign-up__logo">
+            <img src={logo} className="sign-up__logo-img" alt="Логотип" />
+            <h2 className="sign-up__name">BookFlow</h2>
+          </div>
+          <h2 className="sign-up__title">Войти</h2>
+          <form className="sign-up__form">
+            <TextField
+              className="sign-up__input"
+              label="Email"
+              type="text"
+              size="xs"
+              view="clear"
+            />
+
+            <TextField
+              className="sign-up__input"
+              label="Пароль"
+              size="xs"
+              type="password"
+              view="clear"
+            />
+            <div className="sign-up__button-container">
+              <Button
+                className="sign-up__button"
+                type="submit"
+                label="Войти"
+                form="round"
+              />
+            </div>
+          </form>
+          <div>
+            <p className="sign-in__question">
+              У вас нет аккаунта?{" "}
+              <Link className="sign-in__link" to="/sign-up">
+                Зарегистрироваться
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="sign-up__background-img" />
+    </section>
+  );
+};
+
+export default SignIn;
