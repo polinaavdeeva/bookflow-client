14929efbba28a2bcf163859260b92c529dd49801
Олег Дубовой <olegdubovoyy@gmail.com>
2024-04-20 14:59:45 +0300

    Fix: background

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -7,11 +7,11 @@ import Header from '../Header/Header';
 
 const MainLayout = () => {
   return (
-    <Layout style={{display: 'flex', background: '#F7EFE5', height: '100vh'}}>
+    <Layout style={{display: 'flex', background: '#F7EFE5', height: '100%'}}>
         <Menu/>  
-        <Layout direction='column' style={{marginLeft: "15%", flexGrow: 1, display: "flex", flexDirection: "column"}}>
+        <Layout direction='column' style={{marginLeft: "15%", flexGrow: 1, display: "flex", flexDirection: "column", height: '200%'}}>
           <Header/>
-          <div style={{marginTop: 60, height: "100%"}}>
+          <div style={{marginTop: 60}}>
             <Outlet/>
             <Footer/>
           </div>
