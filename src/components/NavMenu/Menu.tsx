ef8b8813aace455619659e2442a85d79da44dbaa
Polina Avdeeva <polinaavdeeva55@gmail.com>
2024-04-21 01:33:51 +0300

    feat: добавлен попап добавления книги

--- src/components/NavMenu/Menu.tsx
+++ src/components/NavMenu/Menu.tsx
@@ -1,34 +1,78 @@
-import { Link } from 'react-router-dom'
-import { Text } from '@consta/uikit/Text';
-import { Button } from '@consta/uikit/Button';
-import "./Menu.scss"
-import React from 'react';
-import LogoItem from '../../assets/logoItem';
+import { Link } from "react-router-dom";
+import { Text } from "@consta/uikit/Text";
+import { Button } from "@consta/uikit/Button";
+import "./Menu.scss";
+import React, { FC } from "react";
+import LogoItem from "../../assets/logoItem";
 
-const Menu = () => {
-  return (
-    <div className='menu-vertical'>
-      
-      <div style={{display: "flex", padding: 20}}>
-        <div style={{paddingTop: 2, paddingRight: 8}}><LogoItem/></div>
-        <Text size='xl' className="text-on-top">BookFlow</Text>
-      </div>
-      
-      <br/>
-      <Link to="."><Button className="menu-button" label="Главная" view='clear' size='s' style={{width: "48vh", textAlign: "left"}}/></Link>
-      
-      <Link to="mybooks"><Button label="Мои книги" view='clear' size='s' style={{width: "48vh", textAlign: "left"}}/></Link>
-      <br/>
-      <Link to="myprofile"><Button label="Профиль" view='clear' size='s' style={{width: "48vh", textAlign: "left"}}/></Link>
-      <br/>
-      <Link to="addbook"><Button label="Добавить книгу" view='clear' size='s' style={{width: "48vh", textAlign: "left"}}/></Link>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <Link to="/sign-in"><Button label="Выйти" view='clear' size='s' style={{width: "48vh", textAlign: "left"}}/></Link>
-    </div>
-  )
+interface IMenuProps {
+  onClick: () => void;
 }
 
-export default Menu
+const Menu: FC<IMenuProps> = ({ onClick }) => {
+  return (
+    <div className="menu-vertical">
+      <div style={{ display: "flex", padding: 20 }}>
+        <div style={{ paddingTop: 2, paddingRight: 8 }}>
+          <LogoItem />
+        </div>
+        <Text size="xl" className="text-on-top">
+          BookFlow
+        </Text>
+      </div>
+
+      <br />
+      <Link to=".">
+        <Button
+          className="menu-button"
+          label="Главная"
+          view="clear"
+          size="s"
+          style={{ width: "48vh", textAlign: "left" }}
+        />
+      </Link>
+
+      <Link to="mybooks">
+        <Button
+          label="Мои книги"
+          view="clear"
+          size="s"
+          style={{ width: "48vh", textAlign: "left" }}
+        />
+      </Link>
+      <br />
+      <Link to="myprofile">
+        <Button
+          label="Профиль"
+          view="clear"
+          size="s"
+          style={{ width: "48vh", textAlign: "left" }}
+        />
+      </Link>
+      <br />
+      <Link to="addbook">
+        <Button
+          label="Добавить книгу"
+          view="clear"
+          size="s"
+          style={{ width: "48vh", textAlign: "left" }}
+          onClick={onClick}
+        />
+      </Link>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Link to="/sign-in">
+        <Button
+          label="Выйти"
+          view="clear"
+          size="s"
+          style={{ width: "48vh", textAlign: "left" }}
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Menu;
--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,24 +1,35 @@
-import { Outlet } from 'react-router-dom'
-import React from 'react'
-import Menu from '../NavMenu/Menu'
-import { Layout } from '@consta/uikit/Layout';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
+import { Outlet } from "react-router-dom";
+import React, { FC } from "react";
+import Menu from "../NavMenu/Menu";
+import { Layout } from "@consta/uikit/Layout";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
 
-const MainLayout = () => {
-  return (
-    <Layout style={{display: 'flex', background: '#F7EFE5', height: '100%vh', width: '98.5vw', padding: 0}}>
-      <div>
-        <Menu/> 
-        <Header/>
-      </div>  
-       <div style={{marginLeft: 237, marginTop: 60}}>
-        <Outlet/>
-        <Footer/>
-      </div>
-      
-    </Layout>
-  )
+interface IMainLayoutProps {
+  onClick: () => void;
 }
 
-export default MainLayout
+const MainLayout: FC<IMainLayoutProps> = ({ onClick }) => {
+  return (
+    <Layout
+      style={{
+        display: "flex",
+        background: "#F7EFE5",
+        height: "100%vh",
+        width: "98.5vw",
+        padding: 0,
+      }}
+    >
+      <div>
+        <Menu onClick={onClick} />
+        <Header />
+      </div>
+      <div style={{ marginLeft: 237, marginTop: 60 }}>
+        <Outlet />
+        <Footer />
+      </div>
+    </Layout>
+  );
+};
+
+export default MainLayout;
