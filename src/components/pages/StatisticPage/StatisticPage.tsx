af5e1125d7a06a33a9d861d9913384e9c404a8b8
Polina Avdeeva <polinaavdeeva55@gmail.com>
2024-06-21 00:39:45 +0300

    статистика

--- src/components/pages/StatisticPage/StatisticPage.tsx
+++ src/components/pages/StatisticPage/StatisticPage.tsx
@@ -99,7 +99,7 @@ const StatisticPage: FC = () => {
       </div>
       <div className="statistic-page__border"></div>
       <div className="statistic-page__chart">
-        <ResponsiveContainer width="100%" height={20}>
+        <ResponsiveContainer width="100%" height={100}>
           <BarChart
             data={chartData}
             margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
